c475fc74fe295f49c9dc42c0eea77dbd894f03f2
Gustavo Silva <guhenriquels@gmail.com>
2023-01-29 19:04:39 -0300

    Add and Updated dashboard

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ db = SQLAlchemy(app)
 class Sheet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String)
     url_sheet = db.Column(db.String, unique=True, nullable=False)
     created = db.Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     updated = db.Column(db.DateTime, onupdate=dt.datetime.utcnow)
@@ -26,14 +27,103 @@ class Table(db.Model):
 with app.app_context():
     db.create_all()
 
-    # db.session.add(Sheet(name="test_name", url_sheet="test_url"))
-    # db.session.commit()
+    sheets = [
+        {
+            "id": 1,
+            "name": "Planejamento Letivo 2023",
+            "description": "Organização para o planejamento letivo de 2023",
+            "url_sheet": "https://google.sheets/1/",
+        },
+        {
+            "id": 2,
+            "name": "Planejamento Letivo 2022",
+            "description": "Já terminado!",
+            "url_sheet": "https://google.sheets/2/",
+        },
+    ]
+
+    tables = [
+        {
+            "id": 1,
+            "name": "Matemática",
+            "range": "A1:M",
+            "sheet_id": 1,
+        },
+        {
+            "id": 2,
+            "name": "Artes",
+            "range": "A1:M",
+            "sheet_id": 1,
+        },
+    ]
+
+    for sheet in sheets:
+        db.session.add(
+            Sheet(
+                id=sheet["id"],
+                name=sheet["name"],
+                description=sheet["description"],
+                url_sheet=sheet["url_sheet"],
+            )
+        )
+
+    db.session.commit()
+
+    for table in tables:
+        db.session.add(
+            Table(
+                id=table["id"],
+                name=table["name"],
+                range=table["range"],
+                sheet_id=table["sheet_id"],
+            )
+        )
+
+    db.session.commit()
 
 
 @app.route("/")
 def home():
-    sheets = db.session.execute(db.select(Sheet).order_by(Sheet.created)).scalars()
-    return render_template("home.html", sheets=sheets)
+    # sheets = db.session.execute(db.select(Sheet).order_by(Sheet.created)).scalars()
+
+    sheets = [
+        {
+            "name": "Planejamento Letivo 2023",
+            "description": "Organização para o planejamento letivo de 2023",
+        },
+        {"name": "Planejamento Letivo 2022", "description": "Já terminado!"},
+    ]
+
+    return render_template(
+        "pages/dashboard_page.html",
+        dash_infos=sheets,
+        dash_name="Planilhas",
+        dash_type="sheets",
+    )
+
+
+@app.route("/tables/")
+def tables():
+
+    tables = [
+        {
+            "name": "Matemática",
+            "range": "A1:M",
+            "sheet_id": 1,
+        },
+        {
+            "name": "Artes",
+            "range": "A1:M",
+            "sheet_id": 1,
+        },
+    ]
+
+    return render_template(
+        "pages/dashboard_page.html",
+        dash_infos=tables,
+        dash_name="Tabelas",
+        dash_type="tables",
+    )
 
 
 if __name__ == "__main__":
